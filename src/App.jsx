665323e2ae23244c125756ab92d5992df62870db
Park Joon Seo <junseu94@naver.com>
2025-08-08 13:41:02 +0900

    상태끌어올리기(2)

--- src/App.jsx
+++ src/App.jsx
@@ -2,85 +2,91 @@ import React, { Component, useState } from "react";
 import "./App.css";
 
 // ----------- 함수형 컴포넌트 ------------
-// function App() {
-//   const [counter, setCounter] = useState(0)
-//   return (
-//   <>
-//       <Count 
-//         counter={counter} 
-//         hello={"hello"} 
-//         array={[1, 2, 3, "안녕하세요"]}
-//       />
-//       <button 
-//         onClick={() => {
-//           setCounter((prev) => prev + 1);
-//         }}
-//         >
-//           +
-//         </button>
-//       <button 
-//         onClick={() => {
-//           setCounter((prev) => prev - 1);
-//         }}
-//         >
-//           -
-//         </button>
-//   </>
-//   )
-// }
+function App() {
+  const [counter, setCounter] = useState(0)
+  return (
+  <>
+      <Count counter={counter} />
+      <PlusButton setCounter={setCounter} />
+      <MinusButton setCounter={setCounter} />
+  </>
+  )
+}
 
-// function Count ({array, counter, hello}) {
-//   console.log("array", array);
-//   console.log("counter", counter);
-//   console.log("hello", hello);
-//   return <div>counter : {counter}</div>
-// }
+function PlusButton({setCounter}) {
+  return (
+    <button 
+      onClick={() => {
+        setCounter((prev) => prev + 1);
+      }}
+    >
+      +
+    </button>
+  );
+}
+
+
+function MinusButton({setCounter}) {
+  return (
+    <button 
+      onClick={() => {
+        setCounter((prev) => prev - 1);
+      }}
+    >
+      -
+    </button>
+  );
+}
+
+function Count ({counter}) {
+  return <div>counter : {counter}</div>
+}
 // ----------------------------------------
 
 // ----------------- 클래스형 컴포넌트 ---------------
-class App extends Component {
-  state = { counter: 2 }
+// class App extends Component {
+//   state = { counter: 2 }
 
-  incrementCounter = () => {
-    this.setState({ counter: this.state.counter + 1});
-  }
-  decrementCounter = () => {
-    this.setState({ counter: this.state.counter - 1});
-  }
+//   incrementCounter = () => {
+//     this.setState({ counter: this.state.counter + 1});
+//   }
+//   decrementCounter = () => {
+//     this.setState({ counter: this.state.counter - 1});
+//   }
 
   
-  render() {
-    return (
-      <>
-        <Count counter={ this.state.counter }/>
-        <PlusButton incrementCounter={this.incrementCounter}
-        />
-        <MinusButton decrementCounter={this.decrementCounter}/>
-      </>
-    );
-  }
-}
-class PlusButton extends Component {
-  render() {
-    return <button onClick={this.props.incrementCounter}>+</button>
-  }
-}
-class MinusButton extends Component {
-  render() {
-    return <button onClick={this.props.decrementCounter}>-</button>
-  }
-}
+//   render() {
+//     return (
+//       <>
+//         <Count counter={ this.state.counter }/>
+//         <PlusButton incrementCounter={this.incrementCounter}
+//         />
+//         <MinusButton decrementCounter={this.decrementCounter}/>
+//       </>
+//     );
+//   }
+// }
+// class PlusButton extends Component {
+//   render() {
+//     return <button onClick={this.props.incrementCounter}>+</button>
+//   }
+// }
+// class MinusButton extends Component {
+//   render() {
+//     return <button onClick={this.props.decrementCounter}>-</button>
+//   }
+// }
 
-class Count extends Component {
-  constructor(props) {
-    super(props);
-  }
+// class Count extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-  render() {
-    console.log("props", this.props);
-    return <div>counter: {this.props.counter}</div>;
-  }
-}
+//   render() {
+//     console.log("props", this.props);
+//     return <div>counter: {this.props.counter}</div>;
+//   }
+// }
 // --------------------------------------------------
 
 export default App;
